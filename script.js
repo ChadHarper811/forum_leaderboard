00d894fea56cf0730b70a24fc3b9953d8828006e
Chad Harper <chadharper811@gmail.com>
2024-10-26 20:08:38 -0500

    added async await fetch request

--- script.js
+++ script.js
@@ -3,4 +3,9 @@ const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
 const postsContainer = document.getElementById("posts-container");
-const fetchData = async () => {};
+const fetchData = async () => {
+    try {
+        const res = await fetch(forumLatest);
+    }
+    catch (err) {}
+};
