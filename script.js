3e08c08834be5c8f0db1698870daf6002ae017b1
Chad Harper <chadharper811@gmail.com>
2024-10-27 11:47:25 -0500

    made new function for posting time stamps

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ const forumLatest = "https://cdn.freecodecamp.org/curriculum/forum-latest/latest
 const forumTopicUrl = "https://forum.freecodecamp.org/t/";
 const forumCategoryUrl = "https://forum.freecodecamp.org/c/";
 const avatarUrl = "https://sea1.discourse-cdn.com/freecodecamp";
+
 const postsContainer = document.getElementById("posts-container");
+
+const timeAgo = (time) => {
+    const currentTime = new Date();
+    const lastPost = new Date(time);
+};
 
 const fetchData = async () => {
     try {
